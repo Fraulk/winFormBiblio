8684332fa90440208d28cfe9d2483b6b7cad06ca
fraulk <fratulker@gmail.com>
2018-12-17 17:03:23 +0100

    ficheAuteur

---
+++ MacDoner/ficheAuteur.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace MacDoner
+{
+    partial class ficheAuteur
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNum = new System.Windows.Forms.Label();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblPrenom = new System.Windows.Forms.Label();
+            this.lblNation = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.txtPrenom = new System.Windows.Forms.TextBox();
+            this.txtNation = new System.Windows.Forms.TextBox();
+            this.btnAnnuler = new System.Windows.Forms.Button();
+            this.btnValider = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNum
+            // 
+            this.lblNum.AutoSize = true;
+            this.lblNum.Location = new System.Drawing.Point(54, 48);
+            this.lblNum.Name = "lblNum";
+            this.lblNum.Size = new System.Drawing.Size(44, 13);
+            this.lblNum.TabIndex = 0;
+            this.lblNum.Text = "Numéro";
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(54, 99);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(29, 13);
+            this.lblNom.TabIndex = 1;
+            this.lblNom.Text = "Nom";
+            // 
+            // lblPrenom
+            // 
+            this.lblPrenom.AutoSize = true;
+            this.lblPrenom.Location = new System.Drawing.Point(54, 154);
+            this.lblPrenom.Name = "lblPrenom";
+            this.lblPrenom.Size = new System.Drawing.Size(43, 13);
+            this.lblPrenom.TabIndex = 2;
+            this.lblPrenom.Text = "Prénom";
+            this.lblPrenom.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblNation
+            // 
+            this.lblNation.AutoSize = true;
+            this.lblNation.Location = new System.Drawing.Point(54, 213);
+            this.lblNation.Name = "lblNation";
+            this.lblNation.Size = new System.Drawing.Size(57, 13);
+            this.lblNation.TabIndex = 3;
+            this.lblNation.Text = "Nationalité";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(140, 45);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 4;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(140, 96);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(100, 20);
+            this.txtNom.TabIndex = 5;
+            // 
+            // txtPrenom
+            // 
+            this.txtPrenom.Location = new System.Drawing.Point(140, 151);
+            this.txtPrenom.Name = "txtPrenom";
+            this.txtPrenom.Size = new System.Drawing.Size(100, 20);
+            this.txtPrenom.TabIndex = 6;
+            // 
+            // txtNation
+            // 
+            this.txtNation.Location = new System.Drawing.Point(140, 210);
+            this.txtNation.Name = "txtNation";
+            this.txtNation.Size = new System.Drawing.Size(100, 20);
+            this.txtNation.TabIndex = 7;
+            // 
+            // btnAnnuler
+            // 
+            this.btnAnnuler.Location = new System.Drawing.Point(57, 279);
+            this.btnAnnuler.Name = "btnAnnuler";
+            this.btnAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnuler.TabIndex = 8;
+            this.btnAnnuler.Text = "Annuler";
+            this.btnAnnuler.UseVisualStyleBackColor = true;
+            // 
+            // btnValider
+            // 
+            this.btnValider.Location = new System.Drawing.Point(165, 279);
+            this.btnValider.Name = "btnValider";
+            this.btnValider.Size = new System.Drawing.Size(75, 23);
+            this.btnValider.TabIndex = 9;
+            this.btnValider.Text = "Valider";
+            this.btnValider.UseVisualStyleBackColor = true;
+            // 
+            // ficheAuteur
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 351);
+            this.Controls.Add(this.btnValider);
+            this.Controls.Add(this.btnAnnuler);
+            this.Controls.Add(this.txtNation);
+            this.Controls.Add(this.txtPrenom);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.lblNation);
+            this.Controls.Add(this.lblPrenom);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.lblNum);
+            this.Name = "ficheAuteur";
+            this.Text = "ficheAuteur";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNum;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblPrenom;
+        private System.Windows.Forms.Label lblNation;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtPrenom;
+        private System.Windows.Forms.TextBox txtNation;
+        private System.Windows.Forms.Button btnAnnuler;
+        private System.Windows.Forms.Button btnValider;
+    }
+}
---
+++ MacDoner/ficheAuteur.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MacDoner
+{
+    public partial class ficheAuteur : Form
+    {
+        private string connexionString = "server=serverbtssiojv.ddns.net;port=3306;Database=ulker_biblio;Uid=ulker;password=ulker";
+        private MySqlConnection maCo;
+        private MySqlCommand maRequete;
+        private MySqlDataReader monReader;
+        public ficheAuteur(bool edit, int num)
+        {
+            InitializeComponent();
+            try
+            {
+                maCo = new MySqlConnection(connexionString);
+                maCo.Open();
+                maRequete = maCo.CreateCommand();
+                maRequete.CommandText = "select * from auteur where num=@paramNum";
+                maRequete.Prepare();
+                maRequete.Parameters.AddWithValue("@paramNum", num);
+                monReader = maRequete.ExecuteReader();
+                if (monReader.Read())
+                {
+                    txtNum.Text = num.ToString();
+                    txtNom.Text = monReader["nom"].ToString();
+                    txtPrenom.Text = monReader["prenom"].ToString();
+                    txtNation.Text = monReader["nationalite"].ToString();
+                }
+                if(edit == false)
+                {
+                    txtNom.Enabled = false;
+                    txtPrenom.Enabled = false;
+                    txtNation.Enabled = false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+                throw;
+            }
+            finally
+            {
+                monReader.Close();
+                maCo.Close();
+            }
+        }
+
+        
+    }
+}
