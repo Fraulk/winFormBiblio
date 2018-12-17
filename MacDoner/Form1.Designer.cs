5ec23b6405b34229d2664f54632caf6186e8f352
fraulk <fratulker@gmail.com>
2018-12-17 15:41:00 +0100

    first form

--- MacDoner/Form1.Designer.cs
+++ MacDoner/Form1.Designer.cs
@@ -28,33 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvListeAuteur = new System.Windows.Forms.DataGridView();
             this.Num = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Nom = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Prenom = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Nation = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnAfficher = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.btnSuprimer = new System.Windows.Forms.Button();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.btnModifier = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListeAuteur)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvListeAuteur
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgvListeAuteur.AllowUserToAddRows = false;
+            this.dgvListeAuteur.AllowUserToDeleteRows = false;
+            this.dgvListeAuteur.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListeAuteur.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Num,
             this.Nom,
             this.Prenom,
             this.Nation});
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(550, 426);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvListeAuteur.Location = new System.Drawing.Point(12, 12);
+            this.dgvListeAuteur.Name = "dgvListeAuteur";
+            this.dgvListeAuteur.ReadOnly = true;
+            this.dgvListeAuteur.Size = new System.Drawing.Size(550, 426);
+            this.dgvListeAuteur.TabIndex = 0;
             // 
             // Num
             // 
@@ -90,62 +90,62 @@
             this.btnAfficher.Text = "Afficher";
             this.btnAfficher.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnSuprimer
             // 
-            this.button2.Location = new System.Drawing.Point(568, 70);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Afficher";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSuprimer.Location = new System.Drawing.Point(568, 70);
+            this.btnSuprimer.Name = "btnSuprimer";
+            this.btnSuprimer.Size = new System.Drawing.Size(75, 23);
+            this.btnSuprimer.TabIndex = 3;
+            this.btnSuprimer.Text = "Suprimer";
+            this.btnSuprimer.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnAjouter
             // 
-            this.button3.Location = new System.Drawing.Point(568, 99);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "Afficher";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnAjouter.Location = new System.Drawing.Point(568, 99);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(75, 23);
+            this.btnAjouter.TabIndex = 4;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnModifier
             // 
-            this.button1.Location = new System.Drawing.Point(568, 41);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Afficher";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnModifier.Location = new System.Drawing.Point(568, 41);
+            this.btnModifier.Name = "btnModifier";
+            this.btnModifier.Size = new System.Drawing.Size(75, 23);
+            this.btnModifier.TabIndex = 5;
+            this.btnModifier.Text = "Modifer";
+            this.btnModifier.UseVisualStyleBackColor = true;
             // 
             // Form_ListeAuteur
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnModifier);
+            this.Controls.Add(this.btnAjouter);
+            this.Controls.Add(this.btnSuprimer);
             this.Controls.Add(this.btnAfficher);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvListeAuteur);
             this.Name = "Form_ListeAuteur";
             this.Text = "Liste d\'auteur";
             this.Load += new System.EventHandler(this.Form_ListeAuteur_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListeAuteur)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvListeAuteur;
         private System.Windows.Forms.DataGridViewTextBoxColumn Num;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nom;
         private System.Windows.Forms.DataGridViewTextBoxColumn Prenom;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nation;
         private System.Windows.Forms.Button btnAfficher;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSuprimer;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.Button btnModifier;
     }
 }
 
--- MacDoner/Form1.cs
+++ MacDoner/Form1.cs
@@ -13,20 +13,51 @@ namespace MacDoner
 {
     public partial class Form_ListeAuteur : Form
     {
-        private string connexionString = "server=serverbtssiojv.ddns.net;port=3306;Database=biblio;Uid=ulker;psw=ulker";
+        private string connexionString = "server=serverbtssiojv.ddns.net;port=3306;Database=ulker_biblio;Uid=ulker;password=ulker";
         private MySqlConnection maCo;
         private MySqlCommand maRequete;
         private MySqlDataReader monReader;
         public Form_ListeAuteur()
         {
             InitializeComponent();
+            RemplirListe();
         }
 
         private void Form_ListeAuteur_Load(object sender, EventArgs e)
         {
-
+            
         }
 
+        private void RemplirListe()
+        {
+            try
+            {
+                maCo = new MySqlConnection(connexionString);
+                maCo.Open();
+                maRequete = maCo.CreateCommand();
+                maRequete.CommandText = "select * from auteur order by nom";
+                monReader = maRequete.ExecuteReader();
+                while (monReader.Read())
+                {
+                    dgvListeAuteur.Rows.Add(
+                        monReader["num"].ToString(),
+                        monReader["nom"].ToString(),
+                        monReader["prenom"].ToString(),
+                        monReader["nationalite"].ToString()
+                        );
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Erreur : " + e.Message);
+                throw;
+            }
+            finally
+            {
+                monReader.Close();
+                maCo.Close();
+            }
+        }
 
 
     }
